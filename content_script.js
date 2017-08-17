fad0dc84e03522c15837b1bcc527e7e918ac69e4
Ruben Marcus <iam@rubenmarcus.com>
2017-08-17 14:46:23 -0300

    change doão joria to prefeitop

--- content_script.js
+++ content_script.js
@@ -26,40 +26,40 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(new RegExp('\\b' + 'João Doria' + '\\b', 'gi'), 'Doão Joria');
-	v = v.replace(new RegExp('\\b' + 'João Dória' + '\\b', 'gi'), 'Doão Jória');
-	v = v.replace(/\bJoao Dória\b/g, "Doão Jória");
-	v = v.replace(/\bJoao Doria\b/g, "Doão Jória");
+	v = v.replace(new RegExp('\\b' + 'João Doria' + '\\b', 'gi'), 'Prefeitop');
+	v = v.replace(new RegExp('\\b' + 'João Dória' + '\\b', 'gi'), 'Prefeitop');
+	v = v.replace(/\bJoao Dória\b/g, "Prefeitop");
+	v = v.replace(/\bJoao Doria\b/g, "Prefeitop");
 
-	v = v.replace(/\bjoao doria\b/g, "doao joria");
-	v = v.replace(/\bjoão doria\b/g, "doão joria");
-	v = v.replace(/\bjoao dória\b/g, "doao jória");
-	v = v.replace(/\bjoão dória\b/g, "doão jória");
+	v = v.replace(/\bjoao doria\b/g, "Prefeitop");
+	v = v.replace(/\bjoão doria\b/g, "Prefeitop");
+	v = v.replace(/\bjoao dória\b/g, "Prefeitop");
+	v = v.replace(/\bjoão dória\b/g, "Prefeitop");
 
-	v = v.replace(/\bJoao doria\b/g, "Doao joria");
-	v = v.replace(/\bJoão doria\b/g, "Doão joria");
-	v = v.replace(/\bJoao dória\b/g, "Doao jória");
-	v = v.replace(/\bJoão dória\b/g, "Doão jória");
+	v = v.replace(/\bJoao doria\b/g, "Prefeitop");
+	v = v.replace(/\bJoão doria\b/g, "Prefeitop");
+	v = v.replace(/\bJoao dória\b/g, "Prefeitop");
+	v = v.replace(/\bJoão dória\b/g, "Prefeitop");
 
-	v = v.replace(/\bjoao Doria\b/g, "doao Joria");
-	v = v.replace(/\bjoão Doria\b/g, "doão Joria");
-	v = v.replace(/\bjoao Dória\b/g, "doao Jória");
-	v = v.replace(/\bjoão Dória\b/g, "doão Jória");
+	v = v.replace(/\bjoao Doria\b/g, "Prefeitop");
+	v = v.replace(/\bjoão Doria\b/g, "Prefeitop");
+	v = v.replace(/\bjoao Dória\b/g, "Prefeitop");
+	v = v.replace(/\bjoão Dória\b/g, "Prefeitop");
 
-	v = v.replace(/\bJOAO DORIA\b/g, "DOÃO JÓRIA");
-	v = v.replace(/\bJOÃO DÓRIA\b/g, "DOÃO JÓRIA");
-	v = v.replace(/\bJOAO DORIA\b/g, "DOAO JORIA");
-	v = v.replace(/\bJOÃO DORIA\b/g, "DOÃO JORIA");
-	v = v.replace(/\bJOÃO DÓRIA\b/g, "DOÃO JÓRIA");
+	v = v.replace(/\bJOAO DORIA\b/g, "Prefeitop");
+	v = v.replace(/\bJOÃO DÓRIA\b/g, "Prefeitop");
+	v = v.replace(/\bJOAO DORIA\b/g, "Prefeitop");
+	v = v.replace(/\bJOÃO DORIA\b/g, "Prefeitop");
+	v = v.replace(/\bJOÃO DÓRIA\b/g, "Prefeitop");
 
-	v = v.replace(/\bdoria\b/g, "joria");
-	v = v.replace(/\bdória\b/g, "jória");
-	v = v.replace(/\bDoria\b/g, "Joria");
-	v = v.replace(/\bDória\b/g, "Jória");
+	v = v.replace(/\bdoria\b/g, "Prefeitop");
+	v = v.replace(/\bdória\b/g, "Prefeitop");
+	v = v.replace(/\bDoria\b/g, "Prefeitop");
+	v = v.replace(/\bDória\b/g, "Prefeitop");
 	
 
-	v = v.replace(/\bGestor\b/g, "Jestor");
-	v = v.replace(/\bgestor\b/g, "jestor");
+	v = v.replace(/\bGestor\b/g, "Prefeitop");
+	v = v.replace(/\bgestor\b/g, "Prefeitop");
 
 	v = v.replace(/\bSérgio Moro\b/g, "$ervo Moro");
 	v = v.replace(/\bsergio moro\b/g, "$ervo Moro");
