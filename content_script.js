66cb46597b496e5a2ccfa09399f44aafa2c4f67d
Ruben Marcus <iam@rubenmarcus.com>
2017-08-17 15:33:33 -0300

    update para PrefeiTOP e add nome completo

--- content_script.js
+++ content_script.js
@@ -26,40 +26,40 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(new RegExp('\\b' + 'João Doria' + '\\b', 'gi'), 'Prefeitop');
-	v = v.replace(new RegExp('\\b' + 'João Dória' + '\\b', 'gi'), 'Prefeitop');
-	v = v.replace(/\bJoao Dória\b/g, "Prefeitop");
-	v = v.replace(/\bJoao Doria\b/g, "Prefeitop");
+	v = v.replace(new RegExp('\\b' + 'João Doria' + '\\b', 'gi'), 'PrefeiTOP');
+	v = v.replace(new RegExp('\\b' + 'João Dória' + '\\b', 'gi'), 'PrefeiTOP');
+	v = v.replace(/\bJoao Dória\b/g, "PrefeiTOP");
+	v = v.replace(/\bJoao Doria\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bjoao doria\b/g, "Prefeitop");
-	v = v.replace(/\bjoão doria\b/g, "Prefeitop");
-	v = v.replace(/\bjoao dória\b/g, "Prefeitop");
-	v = v.replace(/\bjoão dória\b/g, "Prefeitop");
+	v = v.replace(/\bjoao doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoão doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoao dória\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoão dória\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bJoao doria\b/g, "Prefeitop");
-	v = v.replace(/\bJoão doria\b/g, "Prefeitop");
-	v = v.replace(/\bJoao dória\b/g, "Prefeitop");
-	v = v.replace(/\bJoão dória\b/g, "Prefeitop");
+	v = v.replace(/\bJoao doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bJoão doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bJoao dória\b/g, "PrefeiTOP");
+	v = v.replace(/\bJoão dória\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bjoao Doria\b/g, "Prefeitop");
-	v = v.replace(/\bjoão Doria\b/g, "Prefeitop");
-	v = v.replace(/\bjoao Dória\b/g, "Prefeitop");
-	v = v.replace(/\bjoão Dória\b/g, "Prefeitop");
+	v = v.replace(/\bjoao Doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoão Doria\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoao Dória\b/g, "PrefeiTOP");
+	v = v.replace(/\bjoão Dória\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bJOAO DORIA\b/g, "Prefeitop");
-	v = v.replace(/\bJOÃO DÓRIA\b/g, "Prefeitop");
-	v = v.replace(/\bJOAO DORIA\b/g, "Prefeitop");
-	v = v.replace(/\bJOÃO DORIA\b/g, "Prefeitop");
-	v = v.replace(/\bJOÃO DÓRIA\b/g, "Prefeitop");
+	v = v.replace(/\bJOAO DORIA\b/g, "PrefeiTOP");
+	v = v.replace(/\bJOÃO DÓRIA\b/g, "PrefeiTOP");
+	v = v.replace(/\bJOAO DORIA\b/g, "PrefeiTOP");
+	v = v.replace(/\bJOÃO DORIA\b/g, "PrefeiTOP");
+	v = v.replace(/\bJOÃO DÓRIA\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bdoria\b/g, "Prefeitop");
-	v = v.replace(/\bdória\b/g, "Prefeitop");
-	v = v.replace(/\bDoria\b/g, "Prefeitop");
-	v = v.replace(/\bDória\b/g, "Prefeitop");
-	
+	v = v.replace(/\bdoria\b/g, "PrefeiTOP");
+	v = v.replace(/\bdória\b/g, "PrefeiTOP");
+	v = v.replace(/\bDoria\b/g, "PrefeiTOP");
+	v = v.replace(/\bDória\b/g, "PrefeiTOP");
+	v = v.replace(/\bJoão Agripino da Costa Dória Junior\b/g, "PrefeiTOP");
 
-	v = v.replace(/\bGestor\b/g, "Prefeitop");
-	v = v.replace(/\bgestor\b/g, "Prefeitop");
+	v = v.replace(/\bGestor\b/g, "PrefeiTOP");
+	v = v.replace(/\bgestor\b/g, "PrefeiTOP");
 
 	v = v.replace(/\bSérgio Moro\b/g, "$ervo Moro");
 	v = v.replace(/\bsergio moro\b/g, "$ervo Moro");
